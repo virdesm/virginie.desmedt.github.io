bd325c8d7b796b724b525a20d3f5bd3b5c658774
virdesm <123631071+virdesm@users.noreply.github.com>
2024-02-02 14:03:34 +0100

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,1 +1,12 @@
+function clic() {
+    compteurClics++;
+    document.getElementById("compteurClics").textContent = compteurClics;
+}
 
+var compteurClics = 0;
+
+document.getElementById("clic").addEventListener("click", clic);
+
+document.getElementById("desactiver").addEventListener("click", function () {
+    document.getElementById("clic").removeEventListener("click", clic);
+});
